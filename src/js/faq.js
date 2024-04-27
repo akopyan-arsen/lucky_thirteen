095449e00ccd208197a65558cd6b883d7fc115e8
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-04-28 01:04:14 +0200

    bug fix

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,11 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import initAccordion from './Libraries/accordion';
 
-  var accordion = new Accordion('.faq-inner-container', {
-    duration: 300,
-    easing: 'ease-in-out',
-    trigger: 'click',
-    multiple: false,
-    activeClass: 'active',
-    disabledClass: 'disabled',
-    openFirst: false
-  });
+var accordion = new Accordion('.faq-tab', {
+  duration: 300,
+  easing: 'ease-in-out',
+  trigger: 'click',
+  multiple: false,
+  activeClass: 'active',
+  disabledClass: 'disabled',
+  openFirst: false,
+});
