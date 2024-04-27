4084b720709f30b9b081d4be880d6759a083681d
Oleksii <shcherbaalex77@gmail.com>
2024-04-27 12:10:49 +0200

    added swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,8 +1,34 @@
-import initAccordion from './accordion';
+import initAccordion from './Libraries/accordion';
+import initSwiper from './Libraries/swiper';
 
 const buildAboutMe = () => {
   initAccordion('.accordion-container.about-me', {
     openOnInit: [0],
+  });
+  initSwiper('.about-me-swiper .swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.about-me-swiper .button-next',
+    },
+    spaceBetween: 0,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1280: {
+        slidesPerView: 6,
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    slideActiveClass: 'swiper-slide-active',
   });
 };
 
