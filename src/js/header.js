feb0a0af8f7a0d675635e2fcb851aab8926cb63f
Legion12021 <146947925+Legion12021@users.noreply.github.com>
2024-04-29 02:15:36 +0300

    Last try header

--- src/js/header.js
+++ src/js/header.js
@@ -1,79 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuButton = document.querySelector('.menu');
-  const menuPanel = document.querySelector('.ac-menu-panel');
-  const openButton = document.querySelector('.burger-btn');
-  const closeButton = document.querySelector('.menu-btn');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+    const mobileMenuModal = document.querySelector('.mobile-menu-modal');
+    const closeMenu = document.querySelector('.mobile-menu-modal .mobile-close-menu');
+    const mobileMenuLinks = document.querySelectorAll('.mobile-menu-nav a');
 
-  const scroll = link => {
-    const targetId = link.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-
-    if (targetElement) {
-      const offsetTop = targetElement.offsetTop - 40;
-      window.scrollTo({
-        top: offsetTop,
-        behavior: 'smooth',
-      });
-    }
-  };
-
-  function openModal() {
-    document.getElementById('myModal').classList.add('is-open');
-  }
-
-  function closeModal() {
-    document.getElementById('myModal').classList.remove('is-open');
-  }
-
-  openButton.addEventListener('click', function () {
-    openModal();
-  });
-
-  closeButton.addEventListener('click', function () {
-    closeModal();
-  });
-
-  menuButton.addEventListener('click', function () {
-    menuPanel.classList.toggle('active');
-  });
-
-  const menuLinks = document.querySelectorAll('.ac-menu-text');
-  menuLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-      scroll(this);
-      menuPanel.classList.remove('active');
+    menuToggle.addEventListener('click', function () {
+        dropdownMenu.classList.toggle('active');
     });
-  });
 
-  // Обробник події для якірних посилань
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (event) {
-      event.preventDefault(); // Скасування стандартної дії
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        const offsetTop = targetElement.offsetTop - 40;
-        window.scrollTo({
-          top: offsetTop,
-          behavior: 'smooth',
+
+    mobileMenuToggle.addEventListener('click', function () {
+        mobileMenuModal.classList.toggle('active');
+    });
+
+    closeMenu.addEventListener('click', function () {
+        mobileMenuModal.classList.toggle('active');
+
+    });
+
+    mobileMenuLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = this.getAttribute('href');
+            mobileMenuModal.classList.toggle('active');
+            setTimeout(function () {
+                window.location.hash = target;
+                const targetElement = document.querySelector(target);
+                if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                }
+            }, 300);
         });
-      }
     });
-  });
 });
-
-const mobmenuLinks = document.querySelectorAll('.menu-list');
-function closeMobileMenu() {
-  document.getElementById('myModal').classList.remove('is-open');
-}
-
-mobmenuLinks.forEach(function (link) {
-  link.addEventListener('click', closeMobileMenu);
-});
-
-window.onclick = function (event) {
-  const modal = document.getElementById('myModal');
-  if (event.target === modal) {
-    document.getElementById('myModal').classList.remove('is-open');
-  }
-};
