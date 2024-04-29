71b47a6714be1417056eaf85d62df37d5e72091c
Arsen Akopyan <tigrikpictures@gmail.com>
2024-04-29 18:54:45 +0300

    npm i

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,4 +5,4 @@ const buildFaq = () => {
   initAccordion('.faq-accordion-second.accordion-container');
 };
 
-export default buildFaq;
+buildFaq();
--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import './js/header';
 import '/js/about-me';
 import './js/covers';
 import './js/projects';
-import buildFaq from './js/faq';
-buildFaq();
+import './js/faq';
